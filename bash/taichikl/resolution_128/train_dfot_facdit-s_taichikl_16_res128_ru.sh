621b1de19460a93fa907cfb7e5d1180c17d1860c
ktncktnc <doremon@gmail.com>
2025-08-14 17:03:21 +1000

    update taichikl resolution 128 script, DiT S

---
+++ bash/taichikl/resolution_128/train_dfot_facdit-s_taichikl_16_res128_ru.sh
@@ -0,0 +1,31 @@
+python main.py \
+    '+name=DFOT_FacDiT-S_TAICHIKL_16_Res128_RU' \
+    'algorithm=dfot_video' \
+    'experiment=video_generation' \
+    \
+    dataset=taichi \
+    dataset.resolution=128 \
+    dataset.max_frames=16 \
+    dataset.latent.suffix=kl_f8_autoencoder \
+    dataset.latent.shape=null \
+    \
+    algorithm/backbone=dit3d_factorized_attention \
+    '@FacDiT/S' \
+    algorithm.backbone.patch_size=2 \
+    \
+    algorithm/vae=kl_autoencoder_preprocessor \
+    algorithm.vae.pretrained_path=stabilityai/sd-vae-ft-ema \
+    algorithm.vae.batch_size=16 \
+    \
+    algorithm.noise_level=random_uniform \
+    algorithm.variable_context.enabled=True \
+    \
+    experiment.training.batch_size=16 \
+    experiment.training.max_steps=200000 \
+    experiment.training.optim.accumulate_grad_batches=4 \
+    \
+    experiment.validation.val_every_n_step=9999999999 \
+    \
+    cluster=a2i2_multigpu \
+    cluster.params.gpu_type=a100 \
+    cluster.params.num_gpus=2
